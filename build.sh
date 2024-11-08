43398337d0f3b633fb487425ea4e04b53c55df94
Ondřej Čertík <ondrej@certik.us>
2024-11-08 09:29:22 -0700

    Update

--- build.sh
+++ build.sh
@@ -3,31 +3,56 @@
 set -ex
 
 FC=gfortran
-FFLAGS="-ffree-line-length-none -cpp -Iinclude/"
+FFLAGS="-ffree-line-length-none -cpp -Iinclude/ -fPIC"
 
-$FC $FFLAGS -c src/Workflow.f90 -o a.o
-$FC $FFLAGS -c src/ArgumentBase.f90 -o a.o
-$FC $FFLAGS -c src/Version.f90 -o a.o
-$FC $FFLAGS -c src/Kinds.f90 -o a.o
-$FC $FFLAGS -c src/OutputUnit.f90 -o a.o
-$FC $FFLAGS -c src/SystemInfo.f90 -o a.o
-$FC $FFLAGS -c src/Timer.f90 -o a.o
-$FC $FFLAGS -c src/String.f90 -o a.o
-$FC $FFLAGS -c src/Warning.f90 -o a.o
-$FC $FFLAGS -c src/Statistics.f90 -o a.o
-$FC $FFLAGS -c src/Options.f90 -o a.o
-$FC $FFLAGS -c src/SteadyStateDetection.f90 -o a.o
-$FC $FFLAGS -c src/Argument.f90 -o a.o
-$FC $FFLAGS -c src/Steps/Setup.f90 -o a.o
-$FC $FFLAGS -c src/Method.f90 -o a.o
-$FC $FFLAGS -c src/Steps/Run.f90 -o a.o
-$FC $FFLAGS -c src/Steps/DryRun.f90 -o a.o
-$FC $FFLAGS -c src/Steps/Compiler.f90 -o a.o
-$FC $FFLAGS -c src/Steps/System.f90 -o a.o
-$FC $FFLAGS -c src/Steps/Steps.f90 -o a.o
-$FC $FFLAGS -c src/Benchmark.f90 -o a.o
+$FC $FFLAGS -c src/Workflow.f90 -o Workflow.o
+$FC $FFLAGS -c src/ArgumentBase.f90 -o ArgumentBase.o
+$FC $FFLAGS -c src/Version.f90 -o Version.o
+$FC $FFLAGS -c src/Kinds.f90 -o Kinds.o
+$FC $FFLAGS -c src/OutputUnit.f90 -o OutputUnit.o
+$FC $FFLAGS -c src/SystemInfo.f90 -o SystemInfo.o
+$FC $FFLAGS -c src/Timer.f90 -o Timer.o
+$FC $FFLAGS -c src/String.f90 -o String.o
+$FC $FFLAGS -c src/Warning.f90 -o Warning.o
+$FC $FFLAGS -c src/Statistics.f90 -o Statistics.o
+$FC $FFLAGS -c src/Options.f90 -o Options.o
+$FC $FFLAGS -c src/SteadyStateDetection.f90 -o SteadyStateDetection.o
+$FC $FFLAGS -c src/Argument.f90 -o Argument.o
+$FC $FFLAGS -c src/Steps/Setup.f90 -o Setup.o
+$FC $FFLAGS -c src/Method.f90 -o Method.o
+$FC $FFLAGS -c src/Steps/Run.f90 -o Run.o
+$FC $FFLAGS -c src/Steps/DryRun.f90 -o DryRun.o
+$FC $FFLAGS -c src/Steps/Compiler.f90 -o Compiler.o
+$FC $FFLAGS -c src/Steps/System.f90 -o System.o
+$FC $FFLAGS -c src/Steps/Steps.f90 -o Steps.o
+$FC $FFLAGS -c src/Benchmark.f90 -o Benchmark.o
 
-$FC $FFLAGS -c examples/simple/Simple.f90 -o a.o
+$FC $FFLAGS -c examples/simple/Simple.f90 -o Simple.o
+$FC $FFLAGS -o Simple \
+    Simple.o \
+    Workflow.o \
+    ArgumentBase.o \
+    Version.o \
+    Kinds.o \
+    OutputUnit.o \
+    SystemInfo.o \
+    Timer.o \
+    String.o \
+    Warning.o \
+    Statistics.o \
+    Options.o \
+    SteadyStateDetection.o \
+    Argument.o \
+    Setup.o \
+    Method.o \
+    Run.o \
+    DryRun.o \
+    Compiler.o \
+    System.o \
+    Steps.o \
+    Benchmark.o
+
+
 
 
 #$FC $FFLAGS -c examples/poisson/rhofunc.f90 -o a.o
